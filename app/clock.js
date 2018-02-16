37861aee037c7d5dc7113fbfbe8b84261d3a57b5
mwmiller75 <mwmiller75@gmail.com>
2018-02-16 15:01:32 -0700

    Fri @ 3pm checkin

---
+++ app/clock.js
@@ -0,0 +1,27 @@
+function updateClock() {
+    debugger
+    var curTime = new Date();
+
+    var curHrs = curTime.getHrs();
+    var curMins = curTime.getMins();
+    var curSecs = curTime.getSecs();
+
+    // Pad the mins and secs with leading zeros, if required
+    curMins = (curMins < 10 ? "0" : "") + curMins;
+    curSecs = (curSecs < 10 ? "0" : "") + curSecs;
+
+    // Choose either "AM" or "PM" as appropriate
+    var timeOfDay = (curHrs < 12) ? "AM" : "PM";
+
+    // Convert the hrs component to 12-hour format if needed
+    curHrs = (curHrs > 12) ? curHrs - 12 : curHrs;
+
+    // Convert an hrs component of "0" to "12"
+    curHrs = (curHrs == 0) ? 12 : curHrs;
+
+    // Compose the string for display
+    var curTimeString = curHrs + ":" + curMins + ":" + curSecs + " " + timeOfDay;
+
+    // Update the time display
+    document.getElementById("clock").innerHTML = curTimeString;
+}
