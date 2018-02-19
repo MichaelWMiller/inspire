8491600d5c9c9d3b5f8c4f04590b34217b850462
mwmiller75 <mwmiller75@gmail.com>
2018-02-18 19:11:44 -0700

    Sunday nite, todo's not working...krap

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -6,25 +6,30 @@ function WeatherController() {
         weatherService.getWeather(function(weather) {
             console.log(weather);
             //What can you do with this weather object?
+
             draw(weather)
         })
     }
 
-    function draw(wth) {
+    function draw(weather) {
+        var wth = weather
         var template = ``
         var weathElem = document.getElementById("weather-here")
         var kelTemp = wth.main['temp']
         var kelLow = wth.main['temp_min']
         var kelHi = wth.main['temp_max']
-        var curTemp = (kelTemp - 273.15) * 9 / 5 + 32
-        var hiTemp = (kelHi - 273.15) * 9 / 5 + 32
-        var loTemp = (kelHi - 273.15) * 9 / 5 + 32
+        var curTemp = Math.round((kelTemp - 273.15) * 9 / 5 + 32)
+        var hiTemp = Math.round((kelHi - 273.15) * 9 / 5 + 32)
+        var loTemp = Math.round((kelHi - 273.15) * 9 / 5 + 32)
 
         template += `
 		<h5><center>Weather for ${wth.name}</center></h5>
         <p>Currently: <span>${curTemp}</span> Hi today: <span>${hiTemp}</span>  Low tonight: <span>${loTemp}</span></p>
-        <p>You can expect:  <span>${weather[0]['description']}</span></p>
+		<p>You can expect:  <span>${wth.weather[0]['description']}</span></p>
+		<button class="btn button btn-info" onclick="app.controllers.weatherController.getWeather()">Update</button>
 		`
         weathElem.innerHTML = template
     }
+
+    this.getWeather()
 }
